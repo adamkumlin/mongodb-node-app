3d136b45082dfbb6b57941c6fbac30afd6486353
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-06-12 22:57:33 +0200

    feat: add get method

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-import { MongoClient } from "mongodb";
+import { Collection, MongoClient } from "mongodb";
+import readline from "readline";
 
 async function run(): Promise<void> {
   // Avoids an exception
@@ -10,17 +11,39 @@ async function run(): Promise<void> {
     console.log("Successfully connected to database.");
   } catch (error) {
     console.log("Error connecting to database: " + error);
+    return;
   }
 
-  const database = client.db("sample_airbnb");
-  const collection = database.collection("listingsAndReviews");
+  console.log("1. Get entries");
+  console.log("2. Add new entry");
+  console.log("3. Update entry");
+  console.log("4. Delete entry");
+  console.log();
 
+  const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+  rl.question("Welcome. Type a number to choose what to do: \n", async (answer) => {
+    const database = client.db("sample_airbnb");
+    if (parseInt(answer) === 1) {
+      await getFirstTenEntries(database.collection("listingsAndReviews"));
+    }
+    rl.close();
+    await client.close();
+  });
+}
+run().catch(console.dir);
+
+async function getFirstTenEntries(collection: Collection<Document>): Promise<void> {
   try {
-    const getAllEntries = (await collection.find().toArray()).slice(0, 10);
+    const entries = (await collection.find().toArray()).slice(0, 10);
 
-    if (getAllEntries !== null) {
-      for (const entry of getAllEntries) {
-        console.log(entry);
+    if (getFirstTenEntries !== null) {
+      for (const entry of entries) {
+        console.log("Id: " + entry._id);
+        console.log("Name: " + entry.name);
+        console.log("Summary: " + entry.summary);
+        console.log("Monthly price: $" + entry.price);
+        console.log("URL: " + entry.listing_url);
         console.log();
       }
     } else {
@@ -28,10 +51,8 @@ async function run(): Promise<void> {
     }
   } catch (error) {
     console.log("Error getting all entries: " + error);
-  } finally {
-    // Always closes the connection, even if an error occurred
-    await client.close();
   }
 }
 
-run().catch(console.dir);
+async function addNewEntry(): Promise<void> {
+}
