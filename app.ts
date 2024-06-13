05b6b6c5f1b62d159d313af07b2bd40258ee6a15
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-06-13 15:47:41 +0200

    feat: add prompt library

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import { Collection, MongoClient } from "mongodb";
-import readline from "readline";
+import prompt from "prompt-sync";
 
 async function run(): Promise<void> {
   // Avoids an exception
@@ -14,36 +14,38 @@ async function run(): Promise<void> {
     return;
   }
 
+  console.log("Welcome. Type a number to choose what to do:");
   console.log("1. Get entries");
   console.log("2. Add new entry");
   console.log("3. Update entry");
   console.log("4. Delete entry");
   console.log();
 
-  const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-
-  rl.question("Welcome. Type a number to choose what to do: \n", async (answer) => {
-    const database = client.db("sample_airbnb");
-    if (parseInt(answer) === 1) {
-      await getFirstTenEntries(database.collection("listingsAndReviews"));
-    }
-    rl.close();
-    await client.close();
-  });
+  prompt.start();
+  prompt.get(["option"]),
+    async (error: any, result: any) => {
+      const database = client.db("sample_airbnb");
+      
+      await console.log(result.option)
+      if (result.option == 1) {
+        await getFirstTenEntries(database.collection("listingsAndReviews"));
+      } else if (parseInt(result.option) === 2) {
+        let date = inputNewEntryData();
+        addNewEntry();
+      }
+    };
+  await client.close();
 }
 run().catch(console.dir);
 
 async function getFirstTenEntries(collection: Collection<Document>): Promise<void> {
   try {
+    // Get the first 10 entries
     const entries = (await collection.find().toArray()).slice(0, 10);
 
     if (getFirstTenEntries !== null) {
       for (const entry of entries) {
-        console.log("Id: " + entry._id);
-        console.log("Name: " + entry.name);
-        console.log("Summary: " + entry.summary);
-        console.log("Monthly price: $" + entry.price);
-        console.log("URL: " + entry.listing_url);
+        console.log(entry);
         console.log();
       }
     } else {
@@ -54,5 +56,18 @@ async function getFirstTenEntries(collection: Collection<Document>): Promise<voi
   }
 }
 
-async function addNewEntry(): Promise<void> {
+async function addNewEntry(): Promise<void> {}
+
+function inputNewEntryData(): string {
+  console.log();
+  console.log("Add a new entry.");
+  console.log();
+  console.log("Date:");
+
+  prompt.start();
+  prompt.get(["date"]),
+    (error: any, result: any) => {
+      return result.date;
+    };
+  return "";
 }
