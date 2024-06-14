47af09fa0791bde3645ba805ad638adc2ebe6445
Adam Kumlin <skola.adamkumlin99@gmail.com>
2024-06-14 23:32:00 +0200

    feat: add delete and create methods

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
-import { Collection, MongoClient } from "mongodb";
-import prompt from "prompt-sync";
+import { Collection, MongoClient, ObjectId } from "mongodb";
 
+interface User {
+  id?: ObjectId;
+  name: string;
+  joinDate: string;
+}
 async function run(): Promise<void> {
   // Avoids an exception
   const client = new MongoClient(process.env.DB_CONNECTION_STRING as string);
@@ -14,60 +18,89 @@ async function run(): Promise<void> {
     return;
   }
 
-  console.log("Welcome. Type a number to choose what to do:");
   console.log("1. Get entries");
   console.log("2. Add new entry");
   console.log("3. Update entry");
   console.log("4. Delete entry");
   console.log();
 
-  prompt.start();
-  prompt.get(["option"]),
-    async (error: any, result: any) => {
-      const database = client.db("sample_airbnb");
-      
-      await console.log(result.option)
-      if (result.option == 1) {
-        await getFirstTenEntries(database.collection("listingsAndReviews"));
-      } else if (parseInt(result.option) === 2) {
-        let date = inputNewEntryData();
-        addNewEntry();
-      }
-    };
-  await client.close();
+  const prompt = require("prompt-sync")();
+
+  const option = prompt("Welcome. Type a number to choose what to do: \n");
+
+  const database = client.db("Users");
+
+  const users = database.collection<User>("Users");
+
+  if (option == 1) {
+    await getFirstTenUsers(users);
+    await client.close();
+  } else if (option == 2) {
+    const date = await inputNewUserData();
+    await addNewUser(date, users);
+    await client.close();
+  } else if (option == 3) {
+    
+  }
+  else if (option == 4) {
+    const id = await prompt("User ID: ");
+    await deleteUser(id, users);
+    await client.close();
+  }
 }
 run().catch(console.dir);
 
-async function getFirstTenEntries(collection: Collection<Document>): Promise<void> {
+async function getFirstTenUsers(collection: Collection<User>): Promise<void> {
   try {
     // Get the first 10 entries
-    const entries = (await collection.find().toArray()).slice(0, 10);
+    const users = (await collection.find().toArray()).slice(0, 10);
 
-    if (getFirstTenEntries !== null) {
-      for (const entry of entries) {
-        console.log(entry);
+    if (getFirstTenUsers !== null) {
+      for (const user of users) {
+        console.log(user);
         console.log();
       }
     } else {
       console.log("Collection is empty.");
     }
   } catch (error) {
-    console.log("Error getting all entries: " + error);
+    console.log("Error getting all users: " + error);
   }
 }
 
-async function addNewEntry(): Promise<void> {}
+async function addNewUser(user: User, collection: Collection<User>): Promise<void> {
+  try {
+    await collection.insertOne({
+      name: user.name,
+      joinDate: user.joinDate,
+    });
+    console.log("User added.");
+  } catch (error) {
+    console.log("Error adding new user: " + error);
+  }
+}
 
-function inputNewEntryData(): string {
+function inputNewUserData(): User {
   console.log();
-  console.log("Add a new entry.");
+  console.log("Add a new user.");
   console.log();
-  console.log("Date:");
 
-  prompt.start();
-  prompt.get(["date"]),
-    (error: any, result: any) => {
-      return result.date;
-    };
-  return "";
+  const prompt = require("prompt-sync")();
+  const name = prompt("Name: ");
+
+  const user: User = {
+    name: name,
+    joinDate: new Date(Date.now()).toDateString(),
+  };
+
+  return user;
+}
+
+async function deleteUser(id: string, collection: Collection<User>) {
+  try {
+    await collection.deleteOne({ _id: new ObjectId(id) });
+    console.log("User deleted.")
+  } catch (error) {
+    console.log("Error deleting user: " + error);
+  }
 }
